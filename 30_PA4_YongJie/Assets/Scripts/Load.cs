a3f8d28d7be55febc807a13a74aa0322f3cabb8e
Randomdudu <45691684+Randomdudu@users.noreply.github.com>
2020-05-05 21:59:54 +0800

    Added Build and tested
    scene changes over now

---
+++ 30_PA4_YongJie/Assets/Scripts/Load.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene("SampleScene");
+    }
+}
